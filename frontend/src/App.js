b0fc08e186118597a988b5156fb5c9e9caf92b42
khloptsev <p.khloptsev@gmail.com>
2022-08-12 19:23:40 +1100

    update App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import MyNavbar from './components/navbar/MyNavbar';
+import Login from './components/pages/Login';
+import NotFound from './components/pages/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Настройте роутинг для двух путей: / и /login.
+// <Login />
+
+const App = () => (
+  <div className="d-flex flex-column h-100">
+    <MyNavbar />
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </div>
+);
 
 export default App;
