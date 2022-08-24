b2807b035fe5a8e923a5e6e9a9ef9ede098f91b6
khloptsev <p.khloptsev@gmail.com>
2022-08-25 09:45:11 +1000

    update imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
-import Main from './components/main/Main.jsx';
+import Main from './components/routes/Main.jsx';
 import Login from './components/routes/Login.jsx';
 import NotFound from './components/routes/NotFound.jsx';
 
