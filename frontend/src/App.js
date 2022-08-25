5083403b134e56025e87a54872177075da16d3c2
khloptsev <p.khloptsev@gmail.com>
2022-08-25 12:39:47 +1000

    delete temp rules, fix arrow func

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import {
   BrowserRouter,
@@ -11,21 +9,17 @@ import Main from './components/routes/Main.jsx';
 import Login from './components/routes/Login.jsx';
 import NotFound from './components/routes/NotFound.jsx';
 
-// Настройте роутинг для двух путей: / и /login.
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}>
-          <Route path="/" element={<Navigate to="login" replace />} />
-          <Route path="login" element={<Login />} />
-          {/* <Route path="signup" element={<SignUp />} /> */}
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />}>
+        <Route path="/" element={<Navigate to="login" replace />} />
+        <Route path="login" element={<Login />} />
+        {/* <Route path="signup" element={<SignUp />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
