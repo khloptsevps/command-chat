95295d51e4d0ff87ce129b3ad9688180ecf17ed3
khloptsev <p.khloptsev@gmail.com>
2022-08-30 23:58:03 +1000

    added content

--- frontend/src/components/pages/ChatPage.jsx
+++ frontend/src/components/pages/ChatPage.jsx
@@ -1,10 +1,23 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth.jsx';
+import { Container, Row, Col } from 'react-bootstrap';
+import storage from '../../utils/storage.js';
 
-const ChatPage = () => (
-  <p>Тут должен быть чат</p>
-);
+const ChatPage = () => {
+  const { username, token } = storage.getItemData();
+  return (
+    <Container
+      className="h-100 my-4 overflow-hidden rounded shadow"
+    >
+      <Row
+        className="h-100 bg-white flex-md-row"
+      >
+        <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light" />
+        <Col className="col p-0 h-100" />
+      </Row>
+    </Container>
+  );
+};
 
 export default ChatPage;
