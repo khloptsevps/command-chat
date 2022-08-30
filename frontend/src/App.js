9908f391aa2590d24d1f35ba2d2936d04675ecf0
khloptsev <p.khloptsev@gmail.com>
2022-08-30 17:14:55 +1000

    updated imports, routes. Added ChatRoute, context.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,57 @@ import React from 'react';
 import {
   BrowserRouter,
   Navigate,
+  useLocation,
   Route,
   Routes,
   Outlet,
 } from 'react-router-dom';
-import Login from './components/routes/Login.jsx';
-import NotFound from './components/routes/NotFound.jsx';
+import PropTypes from 'prop-types';
+import AuthProvider from './components/authProvider/AuthProvider.jsx';
+import useAuth from './hooks/useAuth.jsx';
 import MyNavbar from './components/navbar/MyNavbar.jsx';
+import LoginPage from './components/pages/LoginPage.jsx';
+import ChatPage from './components/pages/ChatPage.jsx';
+import NotFoundPage from './components/pages/NotFoundPage.jsx';
 import Footer from './components/footer/Footer.jsx';
 
+const ChatRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
+
 const App = () => (
-  <div className="d-flex flex-column h-100">
-    <BrowserRouter>
-      <MyNavbar />
-      <Outlet />
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate to="/login" replace />}
-        />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-    <Footer />
-  </div>
+  <AuthProvider>
+    <div className="d-flex flex-column h-100">
+      <BrowserRouter>
+        <MyNavbar />
+        <Outlet />
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <ChatRoute>
+                <ChatPage />
+              </ChatRoute>
+            )}
+          />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  </AuthProvider>
 );
+
+ChatRoute.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]).isRequired,
+};
 
 export default App;
