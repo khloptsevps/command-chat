3d9ea06068fa91e0fd28c6731f3491b49350ebed
khloptsev <p.khloptsev@gmail.com>
2022-08-30 17:11:04 +1000

    added ChatPage.jsx

---
+++ frontend/src/components/pages/ChatPage.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth.jsx';
+
+const ChatPage = () => (
+  <p>Тут должен быть чат</p>
+);
+
+export default ChatPage;
