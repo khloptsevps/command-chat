fc316fb0140a43a75850dae7d253f5a5d4c9acee
khloptsev <p.khloptsev@gmail.com>
2022-08-30 17:02:42 +1000

    add logOut Button

--- frontend/src/components/navbar/MyNavbar.jsx
+++ frontend/src/components/navbar/MyNavbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Button } from 'react-bootstrap';
+import useAuth from '../../hooks/useAuth.jsx';
 
-const MyNavbar = () => (
-  <Navbar className="shadow-sm" bg="white" variant="light" expand="lg">
+const MyNavbar = () => {
+  const { loggedIn, logOut } = useAuth();
+  return (
+    <Navbar className="shadow-sm" bg="white" variant="light" expand="lg">
     <Container>
       <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+      {loggedIn && <Button onClick={() => logOut()}>Выйти</Button>}
     </Container>
   </Navbar>
-);
+  );
+};
 
 export default MyNavbar;
