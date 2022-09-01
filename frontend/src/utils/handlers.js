22532f928555ecb9e1c4714db952672f418dd7d6
khloptsev <p.khloptsev@gmail.com>
2022-09-01 16:06:28 +1000

    update signInFormHandler

--- frontend/src/utils/handlers.js
+++ frontend/src/utils/handlers.js
@@ -1,24 +1,33 @@
+import { toast } from 'react-toastify';
 import axios from 'axios';
 import storage from './storage.js';
+import routes from './routes.js';
 
-// temporary
-const api = '/api/v1/login';
-
-const signInFormHandler = (setAuthFailed, { logIn }) => (
+const signInFormHandler = (setAuthFailed, { logIn }, navigate) => (
   async (values, { resetForm, setSubmitting }) => {
     try {
       setAuthFailed(false);
-      const response = await axios.post(api, values);
+      const response = await axios.post(routes.signInPath(), values);
       storage.setItem(response.data);
       resetForm();
       logIn();
+      toast.success('Успешная авторизация!', {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      navigate('/');
     } catch (error) {
       setSubmitting(false);
       if (error.isAxiosError && error.response.status === 401) {
         setAuthFailed(true);
       }
+      console.log(error);
     }
   }
 );
-
 export default signInFormHandler;
