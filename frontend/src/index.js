abc03154a6d47cb5402154e121c0f6c987061b2a
khloptsev <p.khloptsev@gmail.com>
2022-09-01 22:06:34 +1000

    added Provider, store.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import store from './slices/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+);
