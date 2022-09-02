42a3c83654a4958b1c64bd3733e118226ab59cb9
khloptsev <p.khloptsev@gmail.com>
2022-09-02 14:59:17 +1000

    updated Chat.jsx

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,27 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import {
-  Container, Row, Col, Button,
+  Container, Row, Col,
 } from 'react-bootstrap';
+import ChannelsList from './channels/ChannelsList.jsx';
 
-const Chat = () => (
-  <Container
-    className="h-100 my-4 overflow-hidden rounded shadow"
-  >
-    <Row
-      className="h-100 bg-white flex-md-row"
-    >
-      <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
-        <Container className="d-flex justify-content-between mb-2 ps-4 pe-2">
-          <span>Каналы</span>
-          <Button variant="outline-primary" size="sm">
-            <span>+</span>
-          </Button>
-        </Container>
-      </Col>
-      <Col className="col p-0 h-100" />
-    </Row>
-  </Container>
-);
+// eslint-disable-next-line arrow-body-style
+const Chat = () => {
+  return (
+    <Container className="h-100 my-4 overflow-hidden rounded shadow">
+      <Row className="h-100 bg-white flex-md-row">
+        <ChannelsList />
+        <Col className="col p-0 h-100" />
+      </Row>
+    </Container>
+  );
+};
 
 export default Chat;
