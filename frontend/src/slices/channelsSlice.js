02a8e830eb72e13ae442207c3fc7b12869caa7fd
khloptsev <p.khloptsev@gmail.com>
2022-09-02 15:00:33 +1000

    added reducers

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -1,11 +1,24 @@
+/* eslint-disable no-param-reassign */
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
 
 const channelsAdapter = createEntityAdapter();
 const initialState = channelsAdapter.getInitialState({ currentChannelId: null });
-
+// {body: "asd", channelId: 1, username: "admin", id: 4}
 const channelsSlice = createSlice({
-  name: 'channelsSlice',
+  name: 'channels',
   initialState,
+  reducers: {
+    initChannels: (state, { payload }) => {
+      channelsAdapter.addMany(state, payload.channels);
+      state.currentChannelId = payload.currentChannelId;
+    },
+    switchChannel: (state, { payload }) => {
+      state.currentChannelId = payload.newCurrentId;
+    },
+  },
 });
+
+export const selectors = channelsAdapter.getSelectors((state) => state.channels);
+export const { initChannels, switchChannel } = channelsSlice.actions;
 
 export default channelsSlice.reducer;
