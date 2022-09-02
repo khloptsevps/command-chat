acc9e9fb2878f3a336c83d1a066771ef2c733341
khloptsev <p.khloptsev@gmail.com>
2022-09-02 16:33:51 +1000

    added reducers

--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -4,9 +4,22 @@ import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
 const messagesAdapter = createEntityAdapter();
 const initialState = messagesAdapter.getInitialState();
 
-const channelsSlice = createSlice({
+const messagesSlice = createSlice({
   name: 'messagesSlice',
   initialState,
+  reducers: {
+    initMessages: (state, { payload }) => {
+      // eslint-disable-next-line object-curly-newline
+      // temporary for tests
+      payload.messages.push({
+        body: 'asd', channelId: 1, username: 'admin', id: 4,
+      });
+      messagesAdapter.addMany(state, payload.messages);
+    },
+  },
 });
 
-export default channelsSlice.reducer;
+export const messagesSelector = messagesAdapter.getSelectors((state) => state.messages);
+export const { initMessages } = messagesSlice.actions;
+
+export default messagesSlice.reducer;
