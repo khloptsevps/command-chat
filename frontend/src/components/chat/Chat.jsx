d6b669ebf1fb2d4b30f1809a50bf345cff05aad9
khloptsev <p.khloptsev@gmail.com>
2022-09-02 11:29:12 +1000

    added channels section and button

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Container, Row, Col, Button,
+} from 'react-bootstrap';
 
 const Chat = () => (
   <Container
@@ -8,7 +11,14 @@ const Chat = () => (
     <Row
       className="h-100 bg-white flex-md-row"
     >
-      <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light" />
+      <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
+        <Container className="d-flex justify-content-between mb-2 ps-4 pe-2">
+          <span>Каналы</span>
+          <Button variant="outline-primary" size="sm">
+            <span>+</span>
+          </Button>
+        </Container>
+      </Col>
       <Col className="col p-0 h-100" />
     </Row>
   </Container>
