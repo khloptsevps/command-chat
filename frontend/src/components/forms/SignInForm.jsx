76d4e1f44f228328deda49e40589d8bd4cf4f0d0
khloptsev <p.khloptsev@gmail.com>
2022-09-02 11:28:22 +1000

    remove validation

--- frontend/src/components/forms/SignInForm.jsx
+++ frontend/src/components/forms/SignInForm.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 import MyButton from './formsElements/MyButton.jsx';
 import MyTextInput from './formsElements/MyInput.jsx';
 import signInFormHandler from '../../utils/handlers.js';
@@ -16,10 +15,6 @@ const SignInForm = () => {
     ref.current.select();
   }, [authFailed]);
 
-  const schema = Yup.object({
-    username: Yup.string().required('Обязательное поле'),
-    password: Yup.string().required('Обязательное поле'),
-  });
   const initFormValues = {
     username: '',
     password: '',
@@ -28,7 +23,6 @@ const SignInForm = () => {
   return (
     <Formik
       initialValues={initFormValues}
-      validationSchema={schema}
       onSubmit={signInFormHandler(setAuthFiled, auth, navigate)}
     >
       <Form className="col-12 col-md-6 mt-3 mt-mb-0">
