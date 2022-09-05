91af34f95aa81b0438fc87b642fe738a4517e394
khloptsev <p.khloptsev@gmail.com>
2022-09-05 23:42:12 +1000

    small update container

--- frontend/src/components/cardContainer/CardContainer.jsx
+++ frontend/src/components/cardContainer/CardContainer.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import {
-  Container, Row, Col, Card,
-} from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import myPropTypes from '../../utils/propTypes.js';
 
 const CardContainer = ({ children }) => (
-  <Container bsPrefix="container-fluid h-100">
-    <Row bsPrefix="row justify-content-center align-content-center h-100">
-      <Col bsPrefix="col-12 col-md-8 col-xxl-6">
-        <Card bsPrefix="card shadow-sm">
-          {children}
-        </Card>
+  <div className="container-fluid h-100">
+    <Row className="justify-content-center align-content-center h-100">
+      <Col className="col-12 col-md-8 col-xxl-6">
+        <Card className="shadow-sm">{children}</Card>
       </Col>
     </Row>
-  </Container>
+  </div>
 );
 
 CardContainer.defaultProps = {
