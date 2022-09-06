6cc9960391d29228b1774ee0ee1c4f785939fd52
khloptsev <p.khloptsev@gmail.com>
2022-09-06 14:43:01 +1000

    updated component

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,42 +1,33 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import storage from '../utils/storage.js';
 import Chat from '../components/chat/Chat.jsx';
 import Loader from '../components/loader/Loader.jsx';
 import routes from '../utils/routes.js';
 import { initChannels } from '../slices/channelsSlice.js';
 import { initMessages } from '../slices/messagesSlice.js';
+import useAuth from '../utils/hooks/useAuth.jsx';
+
+const fetchAndInitChatData = async (getAuthHeader, dispatch, setIsLoading) => {
+  try {
+    const response = await axios.get(routes.dataPath(), getAuthHeader());
+    dispatch(initChannels(response.data));
+    dispatch(initMessages(response.data));
+    setIsLoading(false);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const ChatPage = () => {
-  const { username, token } = storage.getItemData();
+  const { getAuthHeader } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchAndInitChatData = async (userToken) => {
-      const config = {
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-        },
-      };
-      try {
-        const response = await axios.get(routes.dataPath(), config);
-        dispatch(initChannels(response.data));
-        dispatch(initMessages(response.data));
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchAndInitChatData(token);
+    fetchAndInitChatData(getAuthHeader, dispatch, setIsLoading);
   }, [dispatch]);
 
-  return (
-    isLoading
-      ? <Loader />
-      : <Chat />
-  );
+  return isLoading ? <Loader /> : <Chat />;
 };
 
 export default ChatPage;
