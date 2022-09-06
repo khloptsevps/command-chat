3d1829601df446709da2d306ea54566cd71991d6
khloptsev <p.khloptsev@gmail.com>
2022-09-06 14:44:07 +1000

    updated jsx, updated submit

--- frontend/src/components/chat/messages/MessagesForm.jsx
+++ frontend/src/components/chat/messages/MessagesForm.jsx
@@ -1,26 +1,43 @@
-/* eslint-disable no-unused-vars */
 import { useFormik } from 'formik';
 import React, { useEffect, useRef } from 'react';
 import * as Yup from 'yup';
 import { Button, Form } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import useAuth from '../../../utils/hooks/useAuth.jsx';
+
+const onSubmit = (username, channelId) => (v, actions) => {
+  const message = {
+    ...v,
+    channelId,
+    username,
+  };
+  console.log(message);
+  actions.resetForm();
+};
 
 const MessagesForm = () => {
+  const channelId = useSelector((state) => state.channels.currentChannelId);
   const input = useRef();
+  const { username } = useAuth();
   useEffect(() => {
     input.current.focus();
-  }, []);
+  }, [channelId]);
   const initialValues = { body: '' };
   const validationSchema = Yup.object({
     body: Yup.string().required('enterMessage'),
   });
-  const onSubmit = (v, actions) => {
-    console.log(v);
-    actions.resetForm();
-  };
-  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: onSubmit(username, channelId),
+  });
   return (
     <div className="mt-auto px-5 py-3">
-      <Form noValidate className="py-1 border rounded-2" onSubmit={formik.handleSubmit}>
+      <Form
+        noValidate
+        className="py-1 border rounded-2"
+        onSubmit={formik.handleSubmit}
+      >
         <div className="input-group has-validation">
           <Form.Control
             ref={input}
@@ -30,8 +47,13 @@ const MessagesForm = () => {
             placeholder="Ведите сообщение..."
             {...formik.getFieldProps('body')}
           />
-          <Button type="submit" size="sm" className="m-1" disabled={!(formik.dirty && formik.isValid)}>
-            <span>Отпрвить</span>
+          <Button
+            type="submit"
+            size="sm"
+            className="m-1"
+            disabled={!(formik.dirty && formik.isValid)}
+          >
+            <span>Отправить</span>
           </Button>
         </div>
       </Form>
