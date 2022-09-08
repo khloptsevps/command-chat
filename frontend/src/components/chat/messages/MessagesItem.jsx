5b5734f5de8abf3bddcd66fdc6d60311f39945f2
khloptsev <p.khloptsev@gmail.com>
2022-09-08 17:14:02 +1000

    fix

--- frontend/src/components/chat/messages/MessagesItem.jsx
+++ frontend/src/components/chat/messages/MessagesItem.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import myPropTypes from '../../../utils/propTypes';
 
-const MessagesItem = ({ body, username }) => (
-  <div className="text-break mb-2">
-    <b>{username}</b>
-    {`: ${body}`}
-  </div>
-);
+const MessagesItem = ({ username, body }) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current?.scrollIntoView();
+  }, []);
+  return (
+    <div ref={ref} className="text-break mb-2">
+      <b>{username}</b>
+      {`: ${body}`}
+    </div>
+  );
+};
+
+MessagesItem.displayName = 'MessagesItem';
 
 MessagesItem.propTypes = {
   body: myPropTypes.body.isRequired,
--- frontend/src/components/chat/messages/MessagesList.jsx
+++ frontend/src/components/chat/messages/MessagesList.jsx
@@ -10,16 +10,23 @@ const MessagesList = () => {
   const messages = useSelector(messagesSelector.selectAll);
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
   const currentChannel = channels.find((channel) => channel.id === currentChannelId);
-  const currenChannelMessages = messages
-    .filter((message) => message.channelId === currentChannelId);
+  const currenChannelMessages = messages.filter(
+    (message) => message.channelId === currentChannelId
+  );
+
   return (
     <>
       <MessagesHeader title={currentChannel.name} length={currenChannelMessages.length} />
-        <div id="messages-box" className="chat-messages overflow-auto px-5">
-          {currenChannelMessages.length > 0 && currenChannelMessages.map((message) => (
-            <MessagesItem key={message.id} body={message.body} username={message.username} />
+      <div id="messages-box" className="chat-messages overflow-auto px-5">
+        {currenChannelMessages.length > 0 &&
+          currenChannelMessages.map((message) => (
+            <MessagesItem
+              key={message.id}
+              body={message.body}
+              username={message.username}
+            />
           ))}
-        </div>
+      </div>
     </>
   );
 };
