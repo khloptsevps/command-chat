4581ddcf1c55af78a122d59cf2da3db7be7f2358
khloptsev <p.khloptsev@gmail.com>
2022-09-20 10:12:21 +1000

    added i18n texts

--- frontend/src/components/navbar/MyNavbar.jsx
+++ frontend/src/components/navbar/MyNavbar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Container, Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import useAuth from '../../utils/hooks/useAuth.jsx';
 
 const MyNavbar = () => {
+  const { t } = useTranslation();
   const { loggedIn, logOut } = useAuth();
   return (
     <Navbar className="shadow-sm" bg="white" variant="light" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          Hexlet Chat
+          {t('navbar.title')}
         </Navbar.Brand>
-        {loggedIn && <Button onClick={() => logOut()}>Выйти</Button>}
+        {loggedIn && (
+          <Button onClick={() => logOut()}>{t('navbar.button')}</Button>
+        )}
       </Container>
     </Navbar>
   );
