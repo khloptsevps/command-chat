ad5a7ff0742bf929d8f6b2ff50cf4d94e2135f74
khloptsev <p.khloptsev@gmail.com>
2022-09-20 13:41:32 +1000

    added new form, added texts

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link, useLocation, Navigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import loginImage from '../__assets__/images/signIn.svg';
 import CardContainer from '../components/cardContainer/CardContainer.jsx';
-import SignInForm from '../components/forms/SignInForm.jsx';
 import MyImage from '../components/image/MyImage.jsx';
 import useAuth from '../utils/hooks/useAuth.jsx';
+import AuthForm from '../components/forms/authForm/AuthForm.jsx';
 
 const LoginPage = () => {
   const { loggedIn } = useAuth();
   const location = useLocation();
+  const { t } = useTranslation();
 
   return loggedIn ? (
     <Navigate to="/" state={{ from: location }} />
   ) : (
     <CardContainer>
       <Card.Body className="row p-5 pb-0">
-        <MyImage imagePath={loginImage} text="Войти" />
-        <SignInForm />
+        <MyImage imagePath={loginImage} text={t('pages.login.imageAlt')} />
+        <AuthForm />
       </Card.Body>
       <Card.Footer className="p-4">
         <div className="text-center">
-          <span>Нет аккаунта? </span>
-          <Link to="../signup">Регистрация</Link>
+          <span>{t('pages.login.footer.question')}</span>
+          <Link to="../signup">{t('pages.login.footer.link')}</Link>
         </div>
       </Card.Footer>
     </CardContainer>
