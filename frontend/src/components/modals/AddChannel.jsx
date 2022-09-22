b044d2743fb42ecccda9de1eb376f11cef5062cb
khloptsev <p.khloptsev@gmail.com>
2022-09-22 13:43:55 +1000

    added title, added form

--- frontend/src/components/modals/AddChannel.jsx
+++ frontend/src/components/modals/AddChannel.jsx
@@ -1,17 +1,19 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import myPropTypes from '../../utils/propTypes';
+import AddChannelForm from '../forms/modalForms/addChannelForm/AddChannelForm.jsx';
 
 const AddChannel = ({ handleClose }) => {
   const { t } = useTranslation();
   return (
     <>
       <Modal.Header closeButton>
-        {t('pages.chat.modals.addChannel.title')}
+        <Modal.Title>{t('pages.chat.modals.addChannel.title')}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Форма добавлия канала!</Modal.Body>
+      <Modal.Body>
+        <AddChannelForm closeModal={handleClose} />
+      </Modal.Body>
     </>
   );
 };
