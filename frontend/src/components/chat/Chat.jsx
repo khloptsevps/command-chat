9c8d7437f1e1d6746ae840e2116e80fc6dcc9e31
khloptsev <p.khloptsev@gmail.com>
2022-09-22 12:32:49 +1000

    updated logic

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,20 +1,13 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ChannelsList from './channels/ChannelsList.jsx';
 import MessagesList from './messages/MessagesList.jsx';
 import MessagesForm from './messages/MessagesForm.jsx';
-import { closeModal } from '../../slices/modalSlice.js';
 import MyModal from '../modals/MyModal.jsx';
 
 const Chat = () => {
-  const dispatch = useDispatch();
-  const hideModal = () => {
-    dispatch(closeModal({ isOpened: false, type: null }));
-  };
   const modal = useSelector((state) => state.modal);
-  console.log(modal);
   return (
     <Container className="h-100 my-4 overflow-hidden rounded shadow">
       <Row className="h-100 bg-white flex-md-row">
@@ -26,7 +19,7 @@ const Chat = () => {
           </div>
         </div>
       </Row>
-      <MyModal modal={modal} handleClose={hideModal} />
+      <MyModal modal={modal} />
     </Container>
   );
 };
