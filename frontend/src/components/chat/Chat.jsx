24c9dc06ea86d4af4e92a222ba8a7e78cf7e7a13
khloptsev <p.khloptsev@gmail.com>
2022-09-22 12:00:19 +1000

    prepare modal

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,21 +1,34 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import ChannelsList from './channels/ChannelsList.jsx';
 import MessagesList from './messages/MessagesList.jsx';
 import MessagesForm from './messages/MessagesForm.jsx';
+import { closeModal } from '../../slices/modalsSlice.js';
+import MyModal from '../modals/MyModal.jsx';
 
-const Chat = () => (
-  <Container className="h-100 my-4 overflow-hidden rounded shadow">
-    <Row className="h-100 bg-white flex-md-row">
-      <ChannelsList />
-      <div className="col p-0 h-100">
-        <div className="d-flex flex-column h-100 p-0">
-          <MessagesList />
-          <MessagesForm />
+const Chat = () => {
+  const dispatch = useDispatch();
+  const hideModal = () => {
+    dispatch(closeModal({ isOpened: false, type: null }));
+  };
+  const modal = useSelector((state) => state.modal);
+  console.log(modal);
+  return (
+    <Container className="h-100 my-4 overflow-hidden rounded shadow">
+      <Row className="h-100 bg-white flex-md-row">
+        <ChannelsList />
+        <div className="col p-0 h-100">
+          <div className="d-flex flex-column h-100 p-0">
+            <MessagesList />
+            <MessagesForm />
+          </div>
         </div>
-      </div>
-    </Row>
-  </Container>
-);
+      </Row>
+      <MyModal modal={modal} handleClose={hideModal} />
+    </Container>
+  );
+};
 
 export default Chat;
