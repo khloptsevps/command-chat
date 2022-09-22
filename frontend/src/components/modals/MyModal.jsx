d44cea4c187f1d02f9b4c18bc12d0c746304f0a9
khloptsev <p.khloptsev@gmail.com>
2022-09-22 12:40:39 +1000

    added propTypes, handleClose func

--- frontend/src/components/modals/MyModal.jsx
+++ frontend/src/components/modals/MyModal.jsx
@@ -1,16 +1,24 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable arrow-body-style */
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { Modal } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { closeModal } from '../../slices/modalSlice';
+import myPropTypes from '../../utils/propTypes';
 import renderModalContent from './renderModalContent';
 
-const MyModal = ({ modal, handleClose }) => {
+const MyModal = ({ modal }) => {
+  const dispatch = useDispatch();
+  const handleClose = () => {
+    dispatch(closeModal({ isOpened: false, type: 'null' }));
+  };
   return (
     <Modal show={modal.isOpened} onHide={handleClose} size="lg" centered>
       {renderModalContent(modal.type, handleClose)}
     </Modal>
   );
+};
+
+MyModal.propTypes = {
+  modal: myPropTypes.object.isRequired,
 };
 
 export default MyModal;
