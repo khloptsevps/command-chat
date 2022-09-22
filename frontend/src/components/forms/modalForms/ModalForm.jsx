c055bc652308e507ded2276f6a21dfa0354fc7ad
khloptsev <p.khloptsev@gmail.com>
2022-09-22 17:02:28 +1000

    added props, added propTypes

--- frontend/src/components/forms/modalForms/ModalForm.jsx
+++ frontend/src/components/forms/modalForms/ModalForm.jsx
@@ -4,18 +4,23 @@ import { Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import MyTextInput from '../MyTextInput.jsx';
 import useModal from '../../../utils/hooks/useModal.jsx';
+import myPropTypes from '../../../utils/propTypes.js';
 
-const ModalForm = () => {
+const ModalForm = ({ errors, isValid }) => {
   const { t } = useTranslation();
   const { type, handleClose } = useModal();
   return (
     <Form>
       <MyTextInput
         label={t(`pages.chat.modals.${type}.label`)}
+        hideLabel={true}
         type="text"
         name="name"
+        id="name"
+        isInvalid={!isValid}
+        className="mb-2"
       >
-        <div className="invalid-feedback"></div>
+        <div className="invalid-feedback">{errors.name}</div>
         <div className="d-flex justify-content-end">
           <Button
             className="me-2 mt-2"
@@ -24,13 +29,18 @@ const ModalForm = () => {
           >
             {t('pages.chat.modals.buttons.chancel')}
           </Button>
-          <Button className="mt-2">
+          <Button className="mt-2" type="submit">
             {t('pages.chat.modals.buttons.send')}
           </Button>
         </div>
       </MyTextInput>
     </Form>
   );
+};
+
+ModalForm.propTypes = {
+  errors: myPropTypes.object.isRequired,
+  isValid: myPropTypes.boolean.isRequired,
 };
 
 export default ModalForm;
