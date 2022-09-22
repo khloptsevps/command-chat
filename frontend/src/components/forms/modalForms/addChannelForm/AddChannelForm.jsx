c7f09a7b25e6b23977f62e6c6c6a043728a209af
khloptsev <p.khloptsev@gmail.com>
2022-09-22 17:04:36 +1000

    added validation schema, update Formik comp

--- frontend/src/components/forms/modalForms/addChannelForm/AddChannelForm.jsx
+++ frontend/src/components/forms/modalForms/addChannelForm/AddChannelForm.jsx
@@ -1,21 +1,35 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+import { useTranslation } from 'react-i18next';
 import ModalForm from '../ModalForm.jsx';
 
-const validationSchema = Yup.object({
-  name: Yup.string().required('Обязательное поле'),
-});
+const initSchema = (t) =>
+  Yup.object({
+    name: Yup.string()
+      .required(t('pages.chat.modals.addChannel.errors.required'))
+      .min(3, t('pages.chat.modals.addChannel.errors.min'))
+      .max(20, t('pages.chat.modals.addChannel.errors.max')),
+  });
 
 const AddChannelForm = () => {
+  const { t } = useTranslation();
   const initValues = {
     name: '',
   };
-  const handleAddChannel = () => {};
+  // todo сделать обработчик формы
+  const handleAddChannel = (v) => {
+    console.log(v);
+  };
   return (
-    <Formik initialValues={initValues} onSubmit={handleAddChannel}>
-      <ModalForm />
+    <Formik
+      initialValues={initValues}
+      validationSchema={initSchema(t)}
+      onSubmit={handleAddChannel}
+      validateOnChange={false}
+      validateOnBlur={false}
+    >
+      {({ errors, isValid }) => <ModalForm errors={errors} isValid={isValid} />}
     </Formik>
   );
 };
