047a38887f194764cb891a94492ad3bf29296723
khloptsev <p.khloptsev@gmail.com>
2022-09-22 14:50:50 +1000

    added useModal hook

--- frontend/src/components/modals/MyModal.jsx
+++ frontend/src/components/modals/MyModal.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
-import { closeModal } from '../../slices/modalSlice';
+import useModal from '../../utils/hooks/useModal.jsx';
 import myPropTypes from '../../utils/propTypes';
 import renderModalContent from './renderModalContent';
 
 const MyModal = ({ modal }) => {
-  const dispatch = useDispatch();
-  const handleClose = () => {
-    dispatch(closeModal({ isOpened: false, type: 'null' }));
-  };
+  const { handleClose } = useModal();
+
   return (
     <Modal show={modal.isOpened} onHide={handleClose} size="lg" centered>
-      {renderModalContent(modal.type, handleClose)}
+      {renderModalContent(modal.type)}
     </Modal>
   );
 };
