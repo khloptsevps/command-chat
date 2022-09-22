a394b74bcf4b5ccfd6354317696b300527126202
khloptsev <p.khloptsev@gmail.com>
2022-09-22 12:40:52 +1000

    added propTypes

--- frontend/src/components/modals/AddChannel.jsx
+++ frontend/src/components/modals/AddChannel.jsx
@@ -1,9 +1,10 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import myPropTypes from '../../utils/propTypes';
 
-const AddChannel = () => {
+const AddChannel = ({ handleClose }) => {
   const { t } = useTranslation();
   return (
     <>
@@ -13,6 +14,10 @@ const AddChannel = () => {
       <Modal.Body>Форма добавлия канала!</Modal.Body>
     </>
   );
+};
+
+AddChannel.propTypes = {
+  handleClose: myPropTypes.func.isRequired,
 };
 
 export default AddChannel;
