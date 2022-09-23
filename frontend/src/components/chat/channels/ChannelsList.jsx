4afb81d71ad60f6c9a1bb2bce89aecfd3ad6e30e
khloptsev <p.khloptsev@gmail.com>
2022-09-23 11:14:13 +1000

    updated component ChannelsHeader

--- frontend/src/components/chat/channels/ChannelsList.jsx
+++ frontend/src/components/chat/channels/ChannelsList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Col, Nav } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { channelsSelectors } from '../../../slices/channelsSlice.js';
 import ChannelsHeader from './ChannelsHeader.jsx';
@@ -8,10 +7,9 @@ import ChannelsItem from './ChannelsItem.jsx';
 
 const ChannelsList = () => {
   const channels = useSelector(channelsSelectors.selectAll);
-  const { t } = useTranslation();
   return (
     <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
-      <ChannelsHeader title={t('pages.chat.channels.title')} />
+      <ChannelsHeader />
       <Nav as="ul" fill variant="pills" className="px-2">
         {channels.map(({ id, name, removable }) => (
           <ChannelsItem key={id} id={id} name={name} removable={removable} />
