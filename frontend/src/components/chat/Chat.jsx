132b8f64f678eb2827fea41e41ac82ed078668e0
khloptsev <p.khloptsev@gmail.com>
2022-09-23 11:14:53 +1000

    added subscribes

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ChannelsList from './channels/ChannelsList.jsx';
 import MessagesList from './messages/MessagesList.jsx';
 import MessagesForm from './messages/MessagesForm.jsx';
 import MyModal from '../modals/MyModal.jsx';
 import ModalProvider from '../providers/ModalProvider.jsx';
+import socket from '../../utils/socket.js';
+import { addChannel } from '../../slices/channelsSlice.js';
+import { addMessage } from '../../slices/messagesSlice.js';
+
+const setSubscribes = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const channelsListener = (payload) => {
+      dispatch(addChannel(payload));
+    };
+    const messagesListener = (payload) => {
+      dispatch(addMessage(payload));
+    };
+    socket.on('newMessage', messagesListener);
+    socket.on('newChannel', channelsListener);
+    return () => {
+      socket.off('newMessage', messagesListener);
+      socket.off('newChannel', channelsListener);
+    };
+  }, [socket]);
+};
 
 const Chat = () => {
   const modal = useSelector((state) => state.modal);
+  setSubscribes();
   return (
     <ModalProvider>
       <Container className="h-100 my-4 overflow-hidden rounded shadow">
