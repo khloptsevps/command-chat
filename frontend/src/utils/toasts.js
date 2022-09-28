060785263c6d980c8c7f41f8386c6d669287b4a4
khloptsev <p.khloptsev@gmail.com>
2022-09-28 12:37:03 +1000

    add new toast

--- frontend/src/utils/toasts.js
+++ frontend/src/utils/toasts.js
@@ -12,6 +12,19 @@ const networkError = () => {
   });
 };
 
+const channelAdd = () => {
+  toast.success('Канал создан', {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
 export default {
   networkError,
+  channelAdd,
 };
