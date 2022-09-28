5e7cf570e80e02ca54ef3fccc62c899d2608982a
khloptsev <p.khloptsev@gmail.com>
2022-09-28 14:51:28 +1000

    added dropdown buttons

--- frontend/src/components/chat/channels/ChannelsItem.jsx
+++ frontend/src/components/chat/channels/ChannelsItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Nav, Button } from 'react-bootstrap';
+import { Nav, Button, Dropdown, ButtonGroup } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import myPropTypes from '../../../utils/propTypes.js';
 import { switchChannel } from '../../../slices/channelsSlice.js';
+import { openModal } from '../../../slices/modalSlice.js';
+import useModal from '../../../utils/hooks/useModal.jsx';
 
-// TODO 2
-// Реализуйте выпадающее меню с кнопками управления каналом;
-
-const ChannelsItem = ({ id, name }) => {
+const ChannelsItem = ({ id, name, removable }) => {
+  const { setType } = useModal();
   const dispatch = useDispatch();
   const currentChannelId = useSelector(
     (state) => state.channels.currentChannelId
@@ -18,7 +18,11 @@ const ChannelsItem = ({ id, name }) => {
     dispatch(switchChannel({ id }));
   };
 
-  return (
+  const removeButtonHandler = () => {
+    dispatch(openModal({ isOpened: true, type: 'removeChannel' }));
+    setType('removeChannel');
+  };
+  return !removable ? (
     <Nav.Item key={id} as="li" className="w-100">
       <Button
         variant={variant}
@@ -29,12 +33,31 @@ const ChannelsItem = ({ id, name }) => {
         {name}
       </Button>
     </Nav.Item>
+  ) : (
+    <Nav.Item key={id} as="li" className="w-100">
+      <Dropdown as={ButtonGroup} className="d-flex">
+        <Button
+          variant={variant}
+          className="w-100 rounded-0 text-start text-truncate"
+          onClick={handleSwitchChannel}
+        >
+          <span className="me-1">#</span>
+          {name}
+        </Button>
+        <Dropdown.Toggle split variant={variant} />
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={removeButtonHandler}>Удалить</Dropdown.Item>
+          <Dropdown.Item>Переименовать</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Nav.Item>
   );
 };
 
 ChannelsItem.propTypes = {
   id: myPropTypes.number.isRequired,
   name: myPropTypes.string.isRequired,
+  removable: myPropTypes.boolean.isRequired,
 };
 
 export default ChannelsItem;
