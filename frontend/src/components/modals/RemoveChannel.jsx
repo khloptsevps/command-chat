c5e1be3f81b5a2d40ba50f06372efc937d62288d
khloptsev <p.khloptsev@gmail.com>
2022-09-28 17:55:47 +1000

    added buttons handlers

--- frontend/src/components/modals/RemoveChannel.jsx
+++ frontend/src/components/modals/RemoveChannel.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import useModal from '../../utils/hooks/useModal.jsx';
+import socket from '../../utils/socket.js';
 
 const RemoveChannel = () => {
+  const { extra } = useSelector((state) => state.modal);
   const { t } = useTranslation();
+  const { handleClose } = useModal();
+  const buttonHandler = () => {
+    socket.volatile.emit('removeChannel', { id: extra.currId });
+    handleClose();
+  };
   return (
     <>
       <Modal.Header closeButton>
@@ -12,10 +21,10 @@ const RemoveChannel = () => {
       <Modal.Body>
         <p>{t('pages.chat.modals.removeChannel.message')}</p>
         <div className="d-flex justify-content-end">
-          <Button variant="secondary" className="me-2">
+          <Button onClick={handleClose} variant="secondary" className="me-2">
             {t('pages.chat.modals.buttons.chancel')}
           </Button>
-          <Button variant="danger">
+          <Button onClick={buttonHandler} variant="danger">
             {t('pages.chat.modals.buttons.remove')}
           </Button>
         </div>
