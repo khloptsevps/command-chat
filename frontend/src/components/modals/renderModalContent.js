2b4cf0fd078b531baf3d793795ecfa296c90cc6e
khloptsev <p.khloptsev@gmail.com>
2022-09-28 14:52:13 +1000

    added remove channel modal

--- frontend/src/components/modals/renderModalContent.js
+++ frontend/src/components/modals/renderModalContent.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AddChannel from './AddChannel.jsx';
+import RemoveChannel from './RemoveChannel.jsx';
 
 const renderModalContent = (type) => {
   switch (type) {
     case 'addChannel':
       return <AddChannel />;
+    case 'removeChannel':
+      return <RemoveChannel />;
     default:
       return null;
   }
