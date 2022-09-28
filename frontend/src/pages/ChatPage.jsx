c1b7dae35cf72b20e8f92e889bd2b15d005185d5
khloptsev <p.khloptsev@gmail.com>
2022-09-28 17:51:16 +1000

    remove initMessages

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -5,14 +5,12 @@ import Chat from '../components/chat/Chat.jsx';
 import Loader from '../components/loader/Loader.jsx';
 import routes from '../utils/routes.js';
 import { initChannels } from '../slices/channelsSlice.js';
-import { initMessages } from '../slices/messagesSlice.js';
 import useAuth from '../utils/hooks/useAuth.jsx';
 
 const fetchAndInitChatData = async (getAuthHeader, dispatch, setIsLoading) => {
   try {
     const response = await axios.get(routes.dataPath(), getAuthHeader());
     dispatch(initChannels(response.data));
-    dispatch(initMessages(response.data));
     setIsLoading(false);
   } catch (error) {
     console.log(error);
