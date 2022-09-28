dc501313a55259b5e8baaf087465ebf15d99cae5
khloptsev <p.khloptsev@gmail.com>
2022-09-28 17:50:51 +1000

    added remove channel reducer

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -16,13 +16,19 @@ const channelsSlice = createSlice({
       state.currentChannelId = payload.id;
     },
     addChannel: channelsAdapter.addOne,
+    removeChannel: (state, { payload }) => {
+      if (state.currentChannelId === payload) {
+        state.currentChannelId = 1;
+      }
+      channelsAdapter.removeOne(state, payload);
+    },
   },
 });
 
 export const channelsSelectors = channelsAdapter.getSelectors(
   (state) => state.channels
 );
-export const { initChannels, switchChannel, addChannel } =
+export const { initChannels, switchChannel, addChannel, removeChannel } =
   channelsSlice.actions;
 
 export default channelsSlice.reducer;
