c6e67c99b12696cae4d9830207de39ba685c5fbc
khloptsev <p.khloptsev@gmail.com>
2022-09-29 11:32:27 +1000

    added new value params

--- frontend/src/components/providers/ModalProvider.jsx
+++ frontend/src/components/providers/ModalProvider.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { closeModal } from '../../slices/modalSlice.js';
 import { ModalContext } from '../../utils/contexts/index.jsx';
 import myPropTypes from '../../utils/propTypes.js';
+import { channelsSelectors } from '../../slices/channelsSlice.js';
 
 const ModalProvider = ({ children }) => {
   const [type, setType] = useState(null);
+  const channelsNames = useSelector(channelsSelectors.selectAll).map(
+    ({ name }) => name
+  );
   const dispatch = useDispatch();
   const handleClose = () => {
     dispatch(closeModal({ isOpened: false, type: null }));
   };
-  const contextValue = { type, handleClose, setType };
+  const contextValue = { type, handleClose, setType, channelsNames };
   return (
     <ModalContext.Provider value={contextValue}>
       {children}
