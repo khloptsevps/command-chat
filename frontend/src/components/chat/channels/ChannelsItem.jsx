683d3d6de5c9dda8be69f0a1c5cae4efa24de438
khloptsev <p.khloptsev@gmail.com>
2022-09-29 11:36:27 +1000

    updated render, TODO, added renameButtonHandler

--- frontend/src/components/chat/channels/ChannelsItem.jsx
+++ frontend/src/components/chat/channels/ChannelsItem.jsx
@@ -6,12 +6,6 @@ import { switchChannel } from '../../../slices/channelsSlice.js';
 import { openModal } from '../../../slices/modalSlice.js';
 import useModal from '../../../utils/hooks/useModal.jsx';
 
-// TODO 3
-// Реализуйте удаление канала (с подтверждением).
-// Удаляться могут только вновь созданные каналы.
-// При удалении канала должны удаляться и его сообщения, а пользователи,
-// находящиеся в удаляемом канале, должны быть перемещены в дефолтный канал;
-
 // TODO Fix buttons texts
 
 const ChannelsItem = ({ id, name, removable }) => {
@@ -37,34 +31,47 @@ const ChannelsItem = ({ id, name, removable }) => {
     setType('removeChannel');
   };
 
-  return !removable ? (
+  const renameButtonHandler = () => {
+    dispatch(
+      openModal({
+        isOpened: true,
+        type: 'renameChannel',
+        extra: { currId: id },
+      })
+    );
+    setType('renameChannel');
+  };
+
+  return (
     <Nav.Item key={id} as="li" className="w-100">
-      <Button
-        variant={variant}
-        className="w-100 rounded-0 text-start"
-        onClick={handleSwitchChannel}
-      >
-        <span className="me-1">#</span>
-        {name}
-      </Button>
-    </Nav.Item>
-  ) : (
-    <Nav.Item key={id} as="li" className="w-100">
-      <Dropdown as={ButtonGroup} className="d-flex">
+      {!removable ? (
         <Button
           variant={variant}
-          className="w-100 rounded-0 text-start text-truncate"
+          className="w-100 rounded-0 text-start"
           onClick={handleSwitchChannel}
         >
           <span className="me-1">#</span>
           {name}
         </Button>
-        <Dropdown.Toggle split variant={variant} />
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={removeButtonHandler}>Удалить</Dropdown.Item>
-          <Dropdown.Item>Переименовать</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+      ) : (
+        <Dropdown as={ButtonGroup} className="d-flex">
+          <Button
+            variant={variant}
+            className="w-100 rounded-0 text-start text-truncate"
+            onClick={handleSwitchChannel}
+          >
+            <span className="me-1">#</span>
+            {name}
+          </Button>
+          <Dropdown.Toggle split variant={variant} />
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={removeButtonHandler}>Удалить</Dropdown.Item>
+            <Dropdown.Item onClick={renameButtonHandler}>
+              Переименовать
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
     </Nav.Item>
   );
 };
