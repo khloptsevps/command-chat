ac406c95a76938e0588d359d38fccfc4e7474b4d
khloptsev <p.khloptsev@gmail.com>
2022-09-29 12:13:49 +1000

    handled remove channel socket status

--- frontend/src/components/modals/RemoveChannel.jsx
+++ frontend/src/components/modals/RemoveChannel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
@@ -6,12 +6,21 @@ import useModal from '../../utils/hooks/useModal.jsx';
 import socket from '../../utils/socket.js';
 
 const RemoveChannel = () => {
+  const [disabled, setDisabled] = useState(false);
   const { extra } = useSelector((state) => state.modal);
   const { t } = useTranslation();
   const { handleClose } = useModal();
   const buttonHandler = () => {
-    socket.volatile.emit('removeChannel', { id: extra.currId });
-    handleClose();
+    setDisabled(true);
+    socket.volatile.emit(
+      'removeChannel',
+      { id: extra.currId },
+      ({ status }) => {
+        if (status === 'ok') {
+          handleClose();
+        }
+      }
+    );
   };
   return (
     <>
@@ -24,7 +33,7 @@ const RemoveChannel = () => {
           <Button onClick={handleClose} variant="secondary" className="me-2">
             {t('pages.chat.modals.buttons.chancel')}
           </Button>
-          <Button onClick={buttonHandler} variant="danger">
+          <Button onClick={buttonHandler} variant="danger" disabled={disabled}>
             {t('pages.chat.modals.buttons.remove')}
           </Button>
         </div>
