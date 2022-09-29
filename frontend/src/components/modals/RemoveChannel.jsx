6200a9791038a3d74e26595961da62108bda8069
khloptsev <p.khloptsev@gmail.com>
2022-09-29 16:55:35 +1000

    added toasts

--- frontend/src/components/modals/RemoveChannel.jsx
+++ frontend/src/components/modals/RemoveChannel.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import useModal from '../../utils/hooks/useModal.jsx';
 import socket from '../../utils/socket.js';
 import RemoveForm from './forms/RemoveForm.jsx';
+import myToasts from '../../utils/toasts.js';
 
 const RemoveChannel = () => {
+  const { t } = useTranslation();
+  const text = t('toasts.channelRemoved');
   const [disabled, setDisabled] = useState(false);
   const { extra } = useSelector((state) => state.modal);
   const { handleClose } = useModal();
@@ -14,6 +18,7 @@ const RemoveChannel = () => {
     socket.volatile.emit('removeChannel', { id }, ({ status }) => {
       if (status === 'ok') {
         handleClose();
+        myToasts({ type: 'success', text });
       }
     });
   };
--- frontend/src/components/modals/RenameChannel.jsx
+++ frontend/src/components/modals/RenameChannel.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import { channelsSelectors } from '../../slices/channelsSlice.js';
 import useModal from '../../utils/hooks/useModal.jsx';
 import socket from '../../utils/socket.js';
 import ModalForm from './forms/ModalForm.jsx';
+import myToasts from '../../utils/toasts.js';
 
 const RenameChannel = () => {
+  const { t } = useTranslation();
+  const text = t('toasts.channelRenamed');
   const [disabled, setDisabled] = useState(false);
   const { handleClose } = useModal();
   const { extra } = useSelector((state) => state.modal);
@@ -21,6 +25,7 @@ const RenameChannel = () => {
     socket.volatile.emit('renameChannel', data, ({ status }) => {
       if (status === 'ok') {
         handleClose();
+        myToasts({ type: 'success', text });
       }
     });
   };
