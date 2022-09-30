eb3e573b93f7b8b6af7ece3130a137361f2dbd36
khloptsev <p.khloptsev@gmail.com>
2022-09-30 16:18:29 +1000

    added props, added feedback

--- frontend/src/components/forms/signUp/SignUpField.jsx
+++ frontend/src/components/forms/signUp/SignUpField.jsx
@@ -3,29 +3,36 @@ import { Form, FloatingLabel } from 'react-bootstrap';
 import { useField } from 'formik';
 import myPropTypes from '../../../utils/propTypes.js';
 
-const SignUpField = forwardRef(({ label, ...props }, ref) => {
-  const [field, meta] = useField(props);
-  const { name } = field;
-  return (
-    <FloatingLabel controlId={name} label={label} className="mb-3">
-      <Form.Control
-        required={true}
-        ref={ref}
-        {...field}
-        {...props}
-        isInvalid={meta.touched && meta.error}
-      />
-      <div placement="right" className="invalid-tooltip">
-        {meta.error}
-      </div>
-    </FloatingLabel>
-  );
-});
+const SignUpField = forwardRef(
+  ({ label, isUserExist, children, ...props }, ref) => {
+    const [field, meta] = useField(props);
+    const { name } = field;
+    return (
+      <FloatingLabel controlId={name} label={label} className="mb-3">
+        <Form.Control
+          required={true}
+          ref={ref}
+          {...field}
+          {...props}
+          isInvalid={(meta.touched && meta.error) || isUserExist}
+        />
+        {meta.error && (
+          <Form.Control.Feedback type="invalid" tooltip>
+            {meta.error}
+          </Form.Control.Feedback>
+        )}
+        {children}
+      </FloatingLabel>
+    );
+  }
+);
 
 SignUpField.displayName = 'SignUpField';
 
 SignUpField.propTypes = {
   label: myPropTypes.string.isRequired,
+  isUserExist: myPropTypes.boolean,
+  children: myPropTypes.children,
 };
 
 export default SignUpField;
