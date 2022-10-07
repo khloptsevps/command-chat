fd6750a8420c7ef602a5ffd311472cff67d68b28
khloptsev <p.khloptsev@gmail.com>
2022-10-07 14:25:55 +1000

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { I18nextProvider } from 'react-i18next';
-import i18next from 'i18next';
-import App from './App';
-import store from './slices/store.js';
-import './locales/i18n.js';
+import initApp from './initApp.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('chat'));
-root.render(
-  <Provider store={store}>
-    <I18nextProvider i18n={i18next}>
-      <App />
-    </I18nextProvider>
-  </Provider>
-);
+const app = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('chat'));
+  const myApp = await initApp();
+  root.render(<React.StrictMode>{myApp}</React.StrictMode>);
+};
+
+app();
