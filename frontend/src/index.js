a1d62ff424bfe16e889b65a746ee7e038f75ec3a
khloptsev <p.khloptsev@gmail.com>
2022-10-07 12:25:48 +1000

    added i18n provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 import App from './App';
 import store from './slices/store.js';
 import './locales/i18n.js';
@@ -9,6 +11,8 @@ import './locales/i18n.js';
 const root = ReactDOM.createRoot(document.getElementById('chat'));
 root.render(
   <Provider store={store}>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </Provider>
 );
