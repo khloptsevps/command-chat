f9995b73d35dac12a685600a7811d2d4c38058ea
khloptsev <p.khloptsev@gmail.com>
2022-10-11 12:54:16 +1000

    added socket

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { io } from 'socket.io-client';
 import initApp from './initApp.jsx';
 
 const app = async () => {
+  const socket = io();
   const root = ReactDOM.createRoot(document.getElementById('chat'));
-  const myApp = await initApp();
+  const myApp = await initApp(socket);
   root.render(<React.StrictMode>{myApp}</React.StrictMode>);
 };
 
